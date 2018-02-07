0db0089e260814b8e838838fbc0a9764e8c6f551
Amy Holt <amy.elizabeth.holt@gmail.com>
2018-02-06 20:28:26 -0700

    Create dinner registration form

--- src/Components/Dinner.js
+++ src/Components/Dinner.js
@@ -4,12 +4,30 @@ class Dinner extends Component {
   render() {
     return (
       <div className="Dinner">
-        <header className="Dinner-header">
-          <h1 className="Dinner-title">DINNER PAGE</h1>
-        </header>
-        <p className="Dinner-intro">
-          To get started, edit <code>src/Dinner.js</code> and save to reload.
-        </p>
+        <h1>Dinner</h1>
+        <p>Details about the dinner</p>
+        <form>
+          <label for="name" aria-label="Name Input">
+            <input id="name" type="text" placeholder="name" aria-required="true"></input>
+          </label>
+          <label for="number" aria-label="Number Input">
+            <input id="number" type="text" placeholder="number in party" aria-required="true">
+            </input>
+          </label>
+          <label for="email" aria-label="Email Input">
+            <input id="email" type="text" placeholder="email" aria-required="true">
+            </input>
+          </label>
+          <label for="credit" aria-label="Credit Card Input">
+            <input id="credit" type="text" placeholder="credit card number" aria-required="true">
+            </input>
+          </label>
+          <label for="submit" aria-label="Submit Form">
+            <button id="submit" type="submit" value="submit" aria-required="true">
+              Submit
+            </button>
+          </label>
+        </form>
       </div>
     );
   }
