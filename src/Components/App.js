4933fdc552c2ca5cd1c6b3dfc9d3f8634267f397
Amy Holt <amy.elizabeth.holt@gmail.com>
2018-02-07 11:23:57 -0700

    Set up containers for all major sections

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,6 +10,63 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <section>
+          <h3>Mission Statement</h3>
+          <p>Puppy kitty ipsum dolor sit good dog foot stick canary. Teeth Mittens grooming vaccine walk swimming nest good boy furry tongue heel furry treats fish.</p>
+        </section>
+
+        <section>
+          <h2>What We Do</h2>
+          <p>
+            Cage run fast kitten dinnertime ball run foot park fleas throw house train licks stick dinnertime window. Yawn litter fish yawn toy pet gate throw Buddy kitty wag tail ball groom crate ferret heel wet nose Rover toys pet supplies. Bird Food treats tongue lick teeth ferret litter box slobbery litter box crate bird small animals yawn small animals shake slobber gimme five toys polydactyl meow. Turtle cage lazy cat foot lazy cat groom canary window tooth brush bedding lazy cat pet supplies turtle water dog shake pet supplies kitty. Walk bird harness wet nose meow harness grooming water dog lol catz water bedding toys bird seed fetch lazy cat. Parakeet scratcher brush biscuit lick dog tooth walk food lazy cat biscuit. Cockatiel Snowball kitten Rover ferret puppy.
+          </p>
+        </section>
+
+        <section>
+          <h2>How You can Help</h2>
+          <h4>Volunteer</h4>
+          <p>Play Dead sit nap lazy dog wet nose Tigger run fast fish lazy cat wagging hamster toy field yawn feathers ferret yawn aquarium.</p>
+          <h4>Donate</h4>
+          <p>Play Dead sit nap lazy dog wet nose Tigger run fast fish lazy cat wagging hamster toy field yawn feathers ferret yawn aquarium.</p>
+          <h4>Come to our Annual Dinner</h4>
+          <p>Play Dead sit nap lazy dog wet nose Tigger run fast fish lazy cat wagging hamster toy field yawn feathers ferret yawn aquarium.</p>
+        </section>
+
+        <section>
+          <h2>Help us Dress Kids..</h2>
+          <button>Donate Now</button>
+        </section>
+
+        <section>
+          <article>
+            <p>Pet Food pet supplies gimme five puppy cage food feathers food heel feathers running pet gate walk lazy dog Spike. Good Boy park lazy dog walk kibble Scooby snacks licks canary. Maine Coon Cat walk catch water dog slobber chew scratcher ID tag litter tuxedo dog house lazy cat park.</p>
+          </article>
+          <article>
+            <p>Pet Food pet supplies gimme five puppy cage food feathers food heel feathers running pet gate walk lazy dog Spike. Good Boy park lazy dog walk kibble Scooby snacks licks canary. Maine Coon Cat walk catch water dog slobber chew scratcher ID tag litter tuxedo dog house lazy cat park.</p>
+          </article>
+          <article>
+            <p>Pet Food pet supplies gimme five puppy cage food feathers food heel feathers running pet gate walk lazy dog Spike. Good Boy park lazy dog walk kibble Scooby snacks licks canary. Maine Coon Cat walk catch water dog slobber chew scratcher ID tag litter tuxedo dog house lazy cat park.</p>
+          </article>
+        </section>
+
+        <section>
+          <p>Sign up for our newsletter to stay up to date</p>
+          <form>
+            <label for="newsletter-sign-up" aria-required="true">
+              <input id="newsletter-sign-up" type="email" placeholder="email address"></input>
+              <button>Submit</button>
+            </label>
+          </form>
+        </section>
+
+        <section>
+          <h2>Annual Dinner</h2>
+          <p> Dinnertime fetch throw feathers fleas tongue lazy cat lick throw kitten parrot hamster wag tail aquarium chew heel good boy lick feathers cockatiel. Wet Nose food ferret vaccine finch vaccination Scooby snacks string wagging barky tail head good boy pet gate meow good boy. Commands shake bird biscuit left paw finch bark ferret bark gimme five turtle fur canary. Water puppy dog lick kisses pet supplies slobber cat bird seed. Food sit biscuit groom tongue cage.</p>
+        </section>
+
+        <footer>
+          <p>Contact</p>
+        </footer>
       </div>
     );
   }
